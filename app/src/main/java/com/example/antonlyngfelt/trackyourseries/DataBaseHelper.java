a90f6f5fa25c1e9720266cec6529e3c8c2b1cbd7
Anton Lyngfelt <anton.lyngfelt@hotmail.com>
2019-06-28 13:29:23 +0200

    V1 test

--- app/src/main/java/com/example/antonlyngfelt/trackyourseries/DataBaseHelper.java
+++ app/src/main/java/com/example/antonlyngfelt/trackyourseries/DataBaseHelper.java
@@ -1,84 +1,80 @@
 package com.example.antonlyngfelt.trackyourseries;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.content.ContentValues;
-import android.database.Cursor;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
-    //information of database
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "serieDB.db";
-    public static final String TABLE_NAME = "Serie";
-    public static final String COLUMN_NAME = "SerieName";
-    public static final String COLUMN_SEASON = "SerieSeason";
-    public static final String COLUMN_EPISODE = "SerieEpisode";
-    //initialize the database
+    public static final String TABLE_NAME_SERIE = "Serie";
+    public static final String COLUMN_ID_SERIE = "ID";
+    public static final String COLUMN_NAME_SERIE = "SerieName";
+    public static final String COLUMN_SEASON_SERIE = "SerieSeason";
+    public static final String COLUMN_EPISODE_SERIE = "SerieEpisode";
+
+
+
     public DataBaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "(" + COLUMN_NAME +
-                "INTEGER PRIMARYKEY," + COLUMN_SEASON + "TEXT "+ COLUMN_EPISODE + "TEXT2)";
-        db.execSQL(CREATE_TABLE);
+        String CREATE_TABLE_SERIE = "CREATE TABLE " + TABLE_NAME_SERIE + " ("+COLUMN_ID_SERIE+" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " + COLUMN_NAME_SERIE +" TEXT, " +
+                  COLUMN_SEASON_SERIE + " TEXT2, "+ COLUMN_EPISODE_SERIE + " TEXT3)";
+        //String CREATE_TABLE_MOVIE = "CREATE TABLE " + TABLE_NAME_SERIE + " ("+COLUMN_ID+" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " + COLUMN_NAME +" TEXT, " +
+            //    COLUMN_SEASON + " TEXT2, "+ COLUMN_EPISODE + " TEXT3)";
+        db.execSQL(CREATE_TABLE_SERIE);
+       // db.execSQL(CREATE_TABLE_MOVIE);
+
     }
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {}
-    public String loadHandler() {
-        String result = "";
-        String query = "Select * FROM " + TABLE_NAME;
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME_SERIE);
+        onCreate(db);
+    }
+    public List<Serie> loadHandler(){
+        List<Serie> wholeList = new LinkedList<>();
+        String query = "SELECT * FROM "+ TABLE_NAME_SERIE;
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-        while (cursor.moveToNext()) {
+        Cursor cursor = db.rawQuery(query,null);
+        while(cursor.moveToNext()){
             int result_0 = cursor.getInt(0);
-            String result_1 = cursor.getString(1);
-            result += String.valueOf(result_0) + " " + result_1 +
-                    System.getProperty("line.separator");
+            String result_1 =cursor.getString(1);
+            int result_2 = cursor.getInt(2);
+            int result_3 = cursor.getInt(3);
+            wholeList.add(new Serie(result_0, result_1,result_2,result_3 ));
         }
         cursor.close();
         db.close();
-        return result;
+        return wholeList;
     }
-    public void addHandler(Serie serie) {
+    public void addHandler(Serie serie){
         ContentValues values = new ContentValues();
-        values.put(COLUMN_NAME, serie.getName());
-        values.put(COLUMN_SEASON, serie.getSeason());
-        values.put(COLUMN_EPISODE,serie.getEpisode());
+        values.put(COLUMN_NAME_SERIE, serie.getName());
+        values.put(COLUMN_SEASON_SERIE, serie.getSeason());
+        values.put(COLUMN_EPISODE_SERIE, serie.getEpisode());
         SQLiteDatabase db = this.getWritableDatabase();
-        db.insert(TABLE_NAME, null, values);
+        db.insert(TABLE_NAME_SERIE, null, values);
         db.close();
     }
-    public Serie findHandler(String seriename) {
-        String query = "Select * FROM " + TABLE_NAME + " WHERE " + COLUMN_NAME + "=" + "'" + seriename + "'";
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-        Serie serie = new Serie();
-        if (cursor.moveToFirst()) {
-            cursor.moveToFirst();
-            serie.setName(cursor.getString(0));
-            serie.setSeason(Integer.parseInt(cursor.getString(1)));
-            serie.setEpisode(Integer.parseInt(cursor.getString(2)));
-            cursor.close();
-        } else {
-            serie = null;
-        }
-        db.close();
-        return serie;
-    }
-    public boolean deleteHandler(String name) {
+    public boolean deleteHandler(String serieName){
         boolean result = false;
-        String query = "Select * FROM " + TABLE_NAME + " WHERE " + COLUMN_NAME + "= '" + String.valueOf(name) + "'";
+        String query = "Select * FROM " + TABLE_NAME_SERIE + " WHERE " + COLUMN_NAME_SERIE + "= '" + serieName + "'";
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(query, null);
         Serie serie = new Serie();
         if (cursor.moveToFirst()) {
-            serie.setName(cursor.getString(0));
-            db.delete(TABLE_NAME, COLUMN_NAME + "=?",
+            serie.setName(cursor.getString(1));
+            db.delete(TABLE_NAME_SERIE, COLUMN_NAME_SERIE + "=?",
                     new String[] {
-                String.valueOf(serie.getName())
+                serie.getName()
             });
             cursor.close();
             result = true;
@@ -86,13 +82,14 @@ public class DataBaseHelper extends SQLiteOpenHelper {
         db.close();
         return result;
     }
-    public boolean updateHandler(String name, int season, int episode) {
-
+    public boolean updateHandler(String serieName, int season, int episode){
+        String query= "SELECT ID WHERE serieName=serieName";
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues args = new ContentValues();
-        args.put(COLUMN_NAME, name);
-        args.put(COLUMN_SEASON, season);
-        args.put(COLUMN_EPISODE, episode);
-        return db.update(TABLE_NAME, args, COLUMN_NAME + "=" + name, null) > 0;
+        //args.put(COLUMN_ID, ID);
+        args.put(COLUMN_NAME_SERIE, serieName);
+        args.put(COLUMN_SEASON_SERIE, season);
+        args.put(COLUMN_EPISODE_SERIE, episode);
+        return db.update(TABLE_NAME_SERIE, args, COLUMN_NAME_SERIE + " = '"+serieName+"'",null)>0;
     }
 }
--- app/src/main/java/com/example/antonlyngfelt/trackyourseries/Serie.java
+++ app/src/main/java/com/example/antonlyngfelt/trackyourseries/Serie.java
@@ -2,46 +2,46 @@ package com.example.antonlyngfelt.trackyourseries;
 
 public class Serie {
 
+    private int serieID;
     private String name;
     private int season;
     private int episode;
 
-    public Serie(){
+    public Serie(){}
 
-    }
-
-    public Serie(String names, int seasons, int episodes){
+    public Serie(int serieID, String names, int seasons, int episodes){
+        this.serieID = serieID;
         this.name = names;
         this.season = seasons;
         this.episode = episodes;
     }
-
+    public void setSerieID(int serieID) {
+        this.serieID = serieID;
+    }
     public void setName(String name) {
         this.name = name;
     }
-
     public void setSeason(int season) {
         this.season = season;
     }
-
     public void setEpisode(int episode) {
         this.episode = episode;
     }
 
+    public int getSerieID() { return this.serieID; }
     public String getName() {
-        return name;
+        return this.name;
     }
-
     public int getSeason() {
-        return season;
+        return this.season;
+    }
+    public int getEpisode() {
+        return this.episode;
     }
 
-    public int getEpisode() {
-        return episode;
-    }
 
     @Override
     public String toString(){
-        return name +" Season: "+ season +" Episode: "+ episode;
+        return name +" S"+ season +" E"+ episode;
     }
 }
