bb7927352b733a6602379e877e29afb9e5ad0850
Anton Lyngfelt <anton.lyngfelt@hotmail.com>
2019-07-04 15:44:53 +0200

    2019-07-04:15:44 Uppdated XML files

--- app/src/main/java/com/example/antonlyngfelt/trackyourseries/PresentResults.java
+++ app/src/main/java/com/example/antonlyngfelt/trackyourseries/PresentResults.java
@@ -19,6 +19,7 @@ public class PresentResults extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentresult);
     }
+
     @Override
     public void onStart(){
         super.onStart();
